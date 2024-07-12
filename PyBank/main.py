aabfe8d523b29a664f2c82d4fd6e3b2ae400d5fe
Nick Fussy <nfussy023@gmail.com>
2024-07-12 02:02:09 -0500

    Initial commit of PyBank code

---
+++ PyBank/main.py
@@ -0,0 +1,64 @@
+import os #to ensure all os can read and open the file
+import csv #to ensure the program can read the csv file
+
+#cd .. | cd Resources/
+budget_data = os.path.join("Resources", "budget_data.csv")
+
+#open and read the budget csv file
+with open(budget_data) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+    # Read the header row first (skip this part if there is no header)
+    csvheader = next(csvreader)
+    print(f"Header: {csvheader}")
+
+    #lists for storing the data
+    profit_and_loss = []
+    months = []
+
+    #reading through each row of the csv file and adding them to the list
+    for row in csvreader:
+        months.append(row[0])
+        profit_and_loss.append(int(row[1]))
+
+    #finding the change in revenue with list comprehension
+    delta_rev = [(int(profit_and_loss[value]) - int(profit_and_loss[value - 1])) for value in profit_and_loss for value in range(1, len(profit_and_loss))]
+
+    #finding total number of months
+    tot_months = len(months)
+
+    #finding the average revenue change
+    avg_rev = sum(delta_rev) / len(delta_rev)
+
+    #finding the largest increase in revenue
+    greatest_inc = max(delta_rev)
+    greatest_inc_month = months[delta_rev.index(greatest_inc) + 1]
+
+    #finding the largest decrease in revenue
+    greatest_dec = min(delta_rev)
+    greatest_dec_month = months[delta_rev.index(greatest_dec) + 1]
+
+    #printing the results
+    print(f"Financial Analysis")
+    print(f"---------------------------------------------------------------------")
+    print(f"Total Months: {tot_months}")
+    print(f"Total: ${sum(profit_and_loss)}")
+    print(f"Average Change: ${round(avg_rev, 2)}")
+    print(f"Greatest Increase in Profits: {greatest_inc_month} ${greatest_inc}")
+    print(f"Greatest Decrease in Profits: {greatest_dec_month} ${greatest_dec}")
+
+
+
+
+
+
+    
+
+        
+
+
+
+
+
+
+
