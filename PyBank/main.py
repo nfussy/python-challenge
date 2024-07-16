ad0e2aa892a3758a32274681e2e8aa28ac696849
Nick Fussy <nfussy023@gmail.com>
2024-07-15 19:24:40 -0500

    Finished version of PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,8 +1,9 @@
 import os #to ensure all os can read and open the file
 import csv #to ensure the program can read the csv file
 
-#cd .. | cd Resources/
+#cd Resources/
 budget_data = os.path.join("Resources", "budget_data.csv")
+analysis = os.path.join("analysis", "analysis.txt")
 
 #open and read the budget csv file
 with open(budget_data) as csvfile:
@@ -32,11 +33,11 @@ with open(budget_data) as csvfile:
 
     #finding the largest increase in revenue
     greatest_inc = max(delta_rev)
-    greatest_inc_month = months[delta_rev.index(greatest_inc) + 1]
-
+    greatest_inc_mon = months[delta_rev.index(greatest_inc) + 1]
+    
     #finding the largest decrease in revenue
     greatest_dec = min(delta_rev)
-    greatest_dec_month = months[delta_rev.index(greatest_dec) + 1]
+    greatest_dec_mon = months[delta_rev.index(greatest_dec) + 1]
 
     #printing the results
     print(f"Financial Analysis")
@@ -44,10 +45,17 @@ with open(budget_data) as csvfile:
     print(f"Total Months: {tot_months}")
     print(f"Total: ${sum(profit_and_loss)}")
     print(f"Average Change: ${round(avg_rev, 2)}")
-    print(f"Greatest Increase in Profits: {greatest_inc_month} ${greatest_inc}")
-    print(f"Greatest Decrease in Profits: {greatest_dec_month} ${greatest_dec}")
+    print(f"Greatest Increase in Profits: {greatest_inc_mon} $({greatest_inc})")
+    print(f"Greatest Decrease in Profits: {greatest_dec_mon} $({greatest_dec})")
 
-
+with open(analysis, 'w') as file:
+    file.write(f"Financial Analysis\n")
+    file.write(f"---------------------------------------------------------------------\n")
+    file.write(f"Total Months: {tot_months}\n")
+    file.write(f"Total: ${sum(profit_and_loss)}\n")
+    file.write(f"Average Change: ${round(avg_rev, 2)}\n")
+    file.write(f"Greatest Increase in Profits: {greatest_inc_mon} $({greatest_inc})\n")
+    file.write(f"Greatest Decrease in Profits: {greatest_dec_mon} $({greatest_dec})\n")
 
 
 
